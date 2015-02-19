652134ed12594582fcaa71f143353252bb4e6d0c
manojo <manojo10386@gmail.com>
2015-02-19 17:01:59 +0100

    staging foldleft

---
+++ src/main/scala/barbedwire/FoldLeft.scala
@@ -0,0 +1,117 @@
+package barbedwire
+
+import scala.virtualization.lms.common._
+
+
+/**
+ * An implementation of foldr/build-like fusion
+ * as a staged interpreter inside LMS
+ *
+ * stuff is encoded as foldLeft instead
+ *
+ * see the following related post: http://manojo.github.io/2015/02/19/staging-foldleft/
+ *
+ * the type signature of foldLeft is
+ *    def foldLeft[A, B](z: B, comb: (B, A) => A)(xs: List[A]) : B
+ *
+ */
+trait FoldLefts extends ScalaOpsPkg with LiftVariables with While {
+
+  /**
+   * a type alias for the combination function for
+   * foldLeft
+   * `A` is the type of elements that pass through the fold
+   * `S` is the type that is eventually computed
+   */
+  type Comb[A, S] = (Rep[S], Rep[A]) => Rep[S]
+
+  /**
+   * We do not want to explicitly materialize
+   * a staged stream at any point. So
+   */
+  abstract class FoldLeft[A: Manifest, S: Manifest] extends ((Rep[S], Comb[A, S]) => Rep[S]) {
+
+    /**
+     * map
+     */
+
+    def map[B: Manifest](f: Rep[A] => Rep[B]) = FoldLeft[B, S] { (z: Rep[S], comb: Comb[B, S]) =>
+      this.apply(
+        z,
+        (acc: Rep[S], elem: Rep[A]) => comb(acc, f(elem))
+      )
+    }
+
+    /**
+     * filter
+     */
+    def filter(p: Rep[A] => Rep[Boolean]) = FoldLeft[A, S] {  (z: Rep[S], comb: Comb[A, S]) =>
+      this.apply(
+        z,
+        (acc: Rep[S], elem: Rep[A]) =>
+          if (p(elem)) comb(acc, elem) else acc
+      )
+    }
+
+    /**
+     * flatMap
+     */
+    def flatMap[B: Manifest](f: Rep[A] => FoldLeft[B, S]) = FoldLeft[B, S] { (z: Rep[S], comb: Comb[B, S]) =>
+      this.apply(
+        z,
+        (acc: Rep[S], elem: Rep[A]) => {
+          val nestedFld = f(elem)
+          nestedFld.apply(acc, comb)
+        }
+      )
+    }
+
+  }
+
+  /**
+   * companion object, makes it easier to
+   * construct folds
+   */
+  object FoldLeft {
+
+    /**
+     * helper function for ease of use
+     */
+    def apply[A: Manifest, S: Manifest](f: (Rep[S], Comb[A, S]) => Rep[S]) = new FoldLeft[A, S] {
+      def apply(z: Rep[S], comb: Comb[A, S]): Rep[S] = f(z, comb)
+    }
+
+    /**
+     * create a fold from list
+     */
+    def fromList[A: Manifest, S: Manifest](ls: Rep[List[A]]) = FoldLeft[A, S] { (z: Rep[S], comb: Comb[A, S]) =>
+
+      var tmpList = ls
+      var tmp = z
+
+      while (!tmpList.isEmpty) {
+        tmp = comb(tmp, tmpList.head)
+        tmpList = tmpList.tail
+      }
+
+      tmp
+    }
+
+    /**
+     * create a fold from a range
+     */
+    def fromRange[S: Manifest](a: Rep[Int], b: Rep[Int]) = FoldLeft[Int, S] { (z: Rep[S], comb: Comb[Int, S]) =>
+
+      var tmpInt = a
+      var tmp = z
+
+      while (tmpInt <= b) {
+        tmp = comb(tmp, tmpInt)
+        tmpInt = tmpInt + 1
+      }
+
+      tmp
+    }
+
+  }
+}
---
+++ src/test/scala/barbedwire/FoldLeftSuite.scala
@@ -0,0 +1,191 @@
+package barbedwire
+
+import scala.virtualization.lms.common._
+import scala.virtualization.lms.internal.Effects
+import lms._
+
+import java.io.PrintWriter
+import java.io.StringWriter
+import java.io.FileOutputStream
+
+/**
+ * Basic test suite for foldleft
+ */
+
+trait FoldLeftProg extends FoldLefts {
+
+  /**
+   * simple foldLeft back into a list
+   */
+  def foldLeftId(in: Rep[Int]): Rep[List[Int]] = {
+    val xs = List(unit(1), unit(2), unit(3))
+    val fld = FoldLeft.fromList[Int, List[Int]](xs)
+
+    fld.apply(List[Int](), (ls, x) => ls ++ List(x))
+
+  }
+
+  /**
+   * map, test if it inlines
+   */
+  def map(in: Rep[Int]): Rep[List[Int]] = {
+    val xs = List(unit(1), unit(2), unit(3))
+    val mapped = FoldLeft.fromList[Int, List[Int]](xs) map (_ * unit(2))
+
+    mapped.apply(List[Int](), (ls, x) => ls ++ List(x))
+  }
+
+  /**
+   * map map, test if it inlines
+   */
+  def mapmap(in: Rep[Int]): Rep[List[Int]] = {
+    val xs = List(unit(1), unit(2), unit(3))
+
+    val mapped = FoldLeft.fromList[Int, List[Int]](xs) map (_ * unit(2))
+    (mapped map (_ + unit(1))).apply(List[Int](), (ls, x) => ls ++ List(x))
+  }
+
+  /**
+   * map map over a range
+   */
+  def mapmapRange(a: Rep[Int], b: Rep[Int]): Rep[List[Int]] = {
+    val xs = FoldLeft.fromRange[List[Int]](a, b)
+
+    val mapped = xs map (_ * unit(2))
+    (mapped map (_ + unit(1))).apply(List[Int](), (ls, x) => ls ++ List(x))
+  }
+
+  /**
+   * filter over a range
+   */
+  def filterRange(a: Rep[Int], b: Rep[Int]): Rep[List[Int]] = {
+    val xs = FoldLeft.fromRange[List[Int]](a, b)
+
+    val filtered = xs filter (_ % unit(2) == unit(1))
+    filtered.apply(List[Int](), (ls, x) => ls ++ List(x))
+  }
+
+  /**
+   * filter map over a range
+   */
+  def filtermapRange(a: Rep[Int], b: Rep[Int]): Rep[List[Int]] = {
+    val xs = FoldLeft.fromRange[List[Int]](a, b)
+
+    val filtered = xs filter (_ % unit(2) == unit(1))
+    filtered.map(_ * unit(3)).apply(List[Int](), (ls, x) => ls ++ List(x))
+  }
+
+
+  /**
+   * flatMap over a range
+   */
+  def flatMapRange(a: Rep[Int], b: Rep[Int]): Rep[List[Int]] = {
+    val xs = FoldLeft.fromRange[List[Int]](a, b)
+
+    val flatMapped = xs flatMap (i => FoldLeft.fromRange[List[Int]](unit(1), i))
+    flatMapped.apply(List[Int](), (ls, x) => ls ++ List(x))
+  }
+
+  /**
+   * flatMap map over a range
+   */
+  def flatMapmapRange(a: Rep[Int], b: Rep[Int]): Rep[List[Int]] = {
+    val xs = FoldLeft.fromRange[List[Int]](a, b)
+
+    val flatMapped = xs flatMap (i => FoldLeft.fromRange[List[Int]](unit(1), i))
+    flatMapped.map(_ * unit(2)).apply(List[Int](), (ls, x) => ls ++ List(x))
+  }
+
+  /**
+   * flatMap filter map over a range
+   */
+  def flatMapfiltermapRange(a: Rep[Int], b: Rep[Int]): Rep[List[Int]] = {
+    val xs = FoldLeft.fromRange[List[Int]](a, b)
+
+    val flatMapped = xs flatMap (i => FoldLeft.fromRange[List[Int]](unit(1), i))
+    val filtered = flatMapped filter (_ % unit(2) == unit(1))
+    filtered.map(_ * unit(3)).apply(List[Int](), (ls, x) => ls ++ List(x))
+  }
+
+
+}
+
+class FoldLeftSuite extends FileDiffSuite {
+
+  val prefix = "test-out/"
+
+  def testFoldLeft = {
+    withOutFile(prefix + "foldleft") {
+      new FoldLeftProg with ScalaOpsPkgExp with WhileExp with MyScalaCompile { self =>
+
+        val codegen = new ScalaCodeGenPkg with ScalaGenWhile { val IR: self.type = self }
+
+        codegen.emitSource(foldLeftId _, "foldLeftId", new java.io.PrintWriter(System.out))
+        codegen.reset
+
+        val testcfoldLeftId = compile(foldLeftId)
+        scala.Console.println(testcfoldLeftId(1))
+        codegen.reset
+
+        codegen.emitSource(map _, "map", new java.io.PrintWriter(System.out))
+        codegen.reset
+
+        val testcMap = compile(map)
+        scala.Console.println(testcMap(1))
+        codegen.reset
+
+        codegen.emitSource(mapmap _, "mapmap", new java.io.PrintWriter(System.out))
+        codegen.reset
+
+        val testcMapmap = compile(mapmap)
+        scala.Console.println(testcMapmap(1))
+        codegen.reset
+
+        codegen.emitSource2(mapmapRange _, "mapmapRange", new java.io.PrintWriter(System.out))
+        codegen.reset
+
+        val testcMapmapRange = compile2(mapmapRange)
+        scala.Console.println(testcMapmapRange(1, 5))
+        codegen.reset
+
+        codegen.emitSource2(filterRange _, "filterRange", new java.io.PrintWriter(System.out))
+        codegen.reset
+
+        val testcFilterRange = compile2(filterRange)
+        scala.Console.println(testcFilterRange(1, 5))
+        codegen.reset
+
+        codegen.emitSource2(filtermapRange _, "filtermapRange", new java.io.PrintWriter(System.out))
+        codegen.reset
+
+        val testcFiltermapRange = compile2(filtermapRange)
+        scala.Console.println(testcFiltermapRange(1, 5))
+        codegen.reset
+
+        codegen.emitSource2(flatMapRange _, "flatMapRange", new java.io.PrintWriter(System.out))
+        codegen.reset
+
+        val testcFlatMapRange = compile2(flatMapRange)
+        scala.Console.println(testcFlatMapRange(1, 5))
+        codegen.reset
+
+        codegen.emitSource2(flatMapmapRange _, "flatMapmapRange", new java.io.PrintWriter(System.out))
+        codegen.reset
+
+        val testcFlatMapmapRange = compile2(flatMapmapRange)
+        scala.Console.println(testcFlatMapmapRange(1, 5))
+        codegen.reset
+
+        codegen.emitSource2(flatMapfiltermapRange _, "flatMapfiltermapRange", new java.io.PrintWriter(System.out))
+        codegen.reset
+
+        val testcFlatMapfiltermapRange = compile2(flatMapfiltermapRange)
+        scala.Console.println(testcFlatMapfiltermapRange(1, 5))
+        codegen.reset
+
+      }
+    }
+
+    assertFileEqualsCheck(prefix + "foldleft")
+  }
+}
