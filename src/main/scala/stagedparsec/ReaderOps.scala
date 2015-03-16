c2ecfcaffd77a28ab74aed8b12bcd429e90b44bc
manojo <manojo10386@gmail.com>
2015-03-16 17:47:57 +0100

    ReaderOps, for staged parser combinators

---
+++ src/main/scala/stagedparsec/ReaderOps.scala
@@ -0,0 +1,135 @@
+package stagedparsec
+
+import lms._
+import scala.virtualization.lms.common._
+import scala.virtualization.lms.internal.GenericCodegen
+import scala.reflect.SourceContext
+
+import java.io.PrintWriter
+import scala.language.implicitConversions
+
+/**
+ * an implementation of `Reader` as staged struct
+ * based on a previous implementation in
+ * `https://github.com/manojo/experiments/`
+ */
+
+/**
+ * creating types, for later reuse
+ */
+abstract class Reader[+T]
+class StringReader extends Reader[Char]
+
+trait ReaderOps extends Base {
+
+  type Elem
+  type Input <: Reader[Elem]
+
+  //need manifests for later
+  implicit val mInput: Manifest[Input]
+
+  implicit class ReaderCls(rdr: Rep[Input]) {
+    def first: Rep[Elem] = rdr_first(rdr)
+    def rest: Rep[Input] = rdr_rest(rdr)
+    def atEnd: Rep[Boolean] = rdr_atEnd(rdr)
+    def offset: Rep[Int] = rdr_offset(rdr)
+  }
+
+  def rdr_first(rdr: Rep[Input]): Rep[Elem]
+  def rdr_rest(rdr: Rep[Input]): Rep[Input]
+  def rdr_atEnd(rdr: Rep[Input]): Rep[Boolean]
+  def rdr_offset(rdr: Rep[Input]): Rep[Int]
+}
+
+trait StringReaderOps
+  extends ReaderOps
+  with ArrayOps
+  with OrderingOps
+  with NumericOps
+  with PrimitiveOps
+  with While
+  with LiftVariables
+  with BooleanOps {
+
+  type Elem = Char
+  type Input = StringReader
+
+  implicit class StringReaderCls(rdr: Rep[StringReader]) extends ReaderCls(rdr) {
+    def input: Rep[Array[Elem]] = rdr_input(rdr)
+
+    def foreach(f: Rep[Char] => Rep[Unit]) = {
+      var tmp = rdr
+      while (!readVar(tmp).atEnd) {
+        f(readVar(tmp).first)
+        tmp = readVar(tmp).rest
+      }
+    }
+  }
+
+  def rdr_first(rdr: Rep[Input]): Rep[Elem] = rdr.input(rdr.offset)
+  def rdr_atEnd(rdr: Rep[Input]): Rep[Boolean] = rdr.offset >= rdr.input.length
+  def rdr_rest(rdr: Rep[Input]): Rep[Input] = StringReader(rdr.input, rdr.offset + unit(1))
+
+  def rdr_input(rdr: Rep[StringReader]): Rep[Array[Char]]
+
+  def StringReader(input: Rep[Array[Char]], offset: Rep[Int] = unit(0)): Rep[StringReader]
+}
+
+trait StringReaderOpsExp
+  extends StringReaderOps
+  with ArrayOpsExpOpt
+  with OrderingOpsExp
+  with NumericOpsExpOpt
+  with PrimitiveOpsExpOpt
+  with StructOpsExpOptCommon
+  with WhileExp
+  with BooleanOpsExpOpt {
+
+  //A bit of a hack: manifest[StringReader] causes a nullPointerException
+  //possibly due to a bug in Scala
+  implicit val mInput: Manifest[Input] =
+    scala.reflect.ManifestFactory.classType[StringReader](classOf[StringReader])
+
+  /**
+   * creating a string reader as a struct
+   */
+  def StringReader(input: Rep[Array[Char]], offset: Rep[Int] = unit(0)) = {
+
+    struct(classTag[StringReader](mInput),
+      "input" -> input,
+      "offset" -> offset
+    )
+  }
+
+  def rdr_input(rdr: Rep[StringReader]): Rep[Array[Char]] =
+    field[Array[Char]](rdr, "input")
+
+  def rdr_offset(rdr: Rep[StringReader]): Rep[Int] =
+    field[Int](rdr, "offset")
+}
+
+trait ReaderGenBase extends GenericCodegen with BaseGenStructOps {
+  val IR: StringReaderOpsExp
+
+  override def remap[A](m: Manifest[A]) = {
+    m.erasure.getSimpleName match {
+      case "StringReader" => IR.structName(m)
+      case _ => super.remap(m)
+    }
+  }
+}
+
+trait ScalaGenReaderOps
+  extends ScalaGenBase
+  with ReaderGenBase
+  with ScalaGenArrayOps
+  with ScalaGenOrderingOps
+  with ScalaGenNumericOps
+  with ScalaGenPrimitiveOps
+  with ScalaGenStructOps
+  with ScalaGenWhile
+  with ScalaGenBooleanOps
+  with ScalaGenVariables {
+    val IR: StringReaderOpsExp
+}
+
---
+++ src/test/scala/stagedparsec/StringReaderSuite.scala
@@ -0,0 +1,85 @@
+package stagedparsec
+
+import lms._
+import scala.virtualization.lms.common._
+import scala.virtualization.lms.internal.Effects
+
+import java.io.PrintWriter
+import java.io.StringWriter
+import java.io.FileOutputStream
+
+trait StringReaderProg extends StringReaderOps with MiscOps {
+
+  //print reader.first
+  def testFirst(in: Rep[Array[Char]]): Rep[Char] = {
+    val rdr: Rep[StringReader] = StringReader(in)
+    rdr.first
+  }
+
+  def testAtEnd(in: Rep[Array[Char]], offset: Rep[Int]): Rep[Boolean] = {
+    val rdr: Rep[StringReader] = StringReader(in, offset)
+    rdr.atEnd
+  }
+
+  //compute rdr.rest and print first
+  def testRest(in: Rep[Array[Char]]): Rep[Char] = {
+    val rdr: Rep[StringReader] = StringReader(in)
+    val rst = rdr.rest
+
+    rst.first
+  }
+
+  def testIteration(in: Rep[Array[Char]]): Rep[Unit] = {
+    val rdr = StringReader(in)
+    rdr.foreach { c => println(c) }
+  }
+}
+
+class StringReaderSuite extends FileDiffSuite {
+
+  val prefix = "test-out/"
+
+  def testStringReader = {
+    withOutFile(prefix + "stringreader") {
+      new StringReaderProg
+        with StringReaderOpsExp
+        with MiscOpsExp
+        with MyScalaCompile { self =>
+
+        val codegen = new ScalaGenReaderOps with ScalaGenMiscOps { val IR: self.type = self }
+
+        codegen.emitSource(testFirst _, "testFirst", new java.io.PrintWriter(System.out))
+        codegen.reset
+
+        val testcFirst = compile(testFirst)
+        scala.Console.println(testcFirst("hello".toArray))
+        codegen.reset
+
+        codegen.emitSource2(testAtEnd _, "testAtEnd", new java.io.PrintWriter(System.out))
+        codegen.reset
+
+        val testcAtEnd = compile2(testAtEnd)
+        scala.Console.println(testcAtEnd("hello".toArray, 0))
+        scala.Console.println(testcAtEnd("hello".toArray, 6))
+        codegen.reset
+
+        codegen.emitSource(testRest _, "testRest", new java.io.PrintWriter(System.out))
+        codegen.reset
+
+        val testcRest = compile(testRest)
+        scala.Console.println(testcRest("hello".toArray))
+        codegen.reset
+
+        codegen.emitSource(testIteration _, "testIteration", new java.io.PrintWriter(System.out))
+        codegen.reset
+
+        val testcIteration = compile(testIteration)
+        testcIteration("hello".toArray)
+        codegen.reset
+
+      }
+    }
+
+    assertFileEqualsCheck(prefix + "stringreader")
+  }
+}
